6efa8dc589dfcdc49b127062ded537de212bbbbe
FaustoSp <faustopasserini2@gmail.com>
2026-04-25 13:01:00 +0200

    Added cloudfront infrastructure. Added frontend.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -24,6 +24,29 @@ resource "aws_s3_bucket_public_access_block" "processed" {
   restrict_public_buckets = true
 }
 
+data "aws_iam_policy_document" "processed_bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.processed.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.processed.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "processed" {
+  bucket = aws_s3_bucket.processed.id
+  policy = data.aws_iam_policy_document.processed_bucket_policy.json
+}
+
 resource "aws_s3_bucket_notification" "raw" {
   bucket = aws_s3_bucket.raw.id
 
